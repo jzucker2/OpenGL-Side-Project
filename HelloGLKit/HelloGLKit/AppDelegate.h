60d312aa2e027c254ce3a0ac76e786505b307517
Jordan Zucker <jordan.zucker@gmail.com>
2011-11-15 14:04:08 -0600

    Uploaded initial project, empty app

---
+++ HelloGLKit/HelloGLKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloGLKit
+//
+//  Created by Jordan Zucker on 11/15/11.
+//  Copyright (c) 2011 University of Illinois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
